f800f93ba481b8d3d06212343894c6a1f20b0885
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2023-12-27 14:30:37 +0800

    init commmit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={() => console.log("Clicked")}
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ app/users/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {});
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th className="text-white">Name</th>
+            <th className="text-white">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersPage;
